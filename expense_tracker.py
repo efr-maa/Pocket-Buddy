c60a638d2e9fc2fce035504e7eaf97758500a9c6
Elizabeth Frimpomaa <elizabethfrimpomaa887@gmail.com>
2026-07-04 01:56:46 -0400

    expense_tracker.py file update

--- expense_tracker.py
+++ expense_tracker.py
@@ -46,10 +46,10 @@ def get_user_expense():
 
   
 
-def save_expense_to_file(expense, expense_file_path):
+def save_expense_to_file(expense: Expense, expense_file_path):
   print(f"📌 Saving user Expense: {expense} to {expense_file_path}")
   with open(expense_file_path, "a") as f:
-    f.write(f"{expense.name},{expense.amount},{expense.category}/n")
+    f.write(f"{expense.name},{expense.amount},{expense.category}\n")
  
 
 def summarize_expense():
